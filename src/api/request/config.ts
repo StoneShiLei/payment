ee6650ead9e5b0684f209209916337f8132b566d
Stone <569812422@qq.com>
2024-12-09 11:40:39 +0800

    4

--- src/api/request/config.ts
+++ src/api/request/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-	// baseURL: 'https://shannengzhixiang.tianrui-e.com',
-	baseURL: 'https://shannengzhixiang.public.hafuhafu.cn:36443',
+	baseURL: 'https://shannengzhixiang.tianrui-e.com',
+	// baseURL: 'https://shannengzhixiang.public.hafuhafu.cn:36443',
 	payBaseURL: 'https://syb.allinpay.com',
 	payURL: 'https://syb.allinpay.com/apiweb/h5unionpay/unionorder'
 }
